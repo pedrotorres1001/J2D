c53ec6f26b0bc398070d470ef34cbeac4e224d05
Rita Guimarães <kikasgui@gmail.com>
2024-11-20 21:43:19 +0000

    Minor fixes
    - Lever fix
    - Player animation fix
    - Grappling hook's rope width adjustment

--- Assets/Scripts/SwitchController.cs
+++ Assets/Scripts/SwitchController.cs
@@ -6,15 +6,34 @@ public class SwitchController : MonoBehaviour
 {
     [SerializeField] private GameObject door;
     [SerializeField] private Sprite switchUsed;
+    private bool isColliding = false;
 
+    private void Start() {
+        isColliding = false;
+    }
 
-    void OnTriggerStay2D(Collider2D other)
+    private void Update() 
     {
-        if(other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.Q)) {
-            if(door != null) {
+        if(isColliding && Input.GetKeyDown(KeyCode.Q)) 
+        {
+            if(door != null) 
+            {
                 door.GetComponent<DoorController>().OpenDoor();
                 gameObject.GetComponent<SpriteRenderer>().sprite = switchUsed;
             }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player")) {
+            isColliding = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player")) {
+            isColliding = false;
         }
     }
 }
