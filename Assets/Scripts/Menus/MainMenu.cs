98f2e46de9d711fe5b46a89b3570695ac34912b9
Pedro Torres <118758354+pedrotorres1001@users.noreply.github.com>
2024-11-28 12:20:38 +0000

    big one

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Village");
     }
 
     public void QuitGame()
