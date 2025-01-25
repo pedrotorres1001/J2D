be6d3beabd5037ff0e0ea924f2f9ba7e4a0041dd
pedrotorres1001 <118758354+pedrotorres1001@users.noreply.github.com>
2025-01-25 02:42:01 +0000

    refactor

--- Assets/Scripts/Enemy/Manti.cs
+++ Assets/Scripts/Enemy/Manti.cs
@@ -12,13 +12,11 @@ public class Manti : Enemy
     public float interval = 1f; // Intervalo em segundos
     private float timer = 0f; // Temporizador interno
 
-
     private bool hasDashed = false;
     // Dash properties
     private Transform player;
     private float distanceToPlayer;
     private Rigidbody2D rb;
-    private bool isDashing;
     private Animator animator;
     private bool colidedWithPlayer;
 
@@ -35,6 +33,9 @@ public class Manti : Enemy
     private EyeCollider eyeCollider;
     private bool canSeePlayer;
 
+    private string state = "patrol";
+    private float stateTimer = 0f;
+
     protected override void Start() {
         base.Start();
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -42,83 +43,65 @@ public class Manti : Enemy
         animator = gameObject.GetComponent<Animator>();
         eyeCollider = gameObject.GetComponentInChildren<EyeCollider>();
 
-        isDashing = false;
         direction = 1;
         colidedWithPlayer = false;
     }
 
     private void Update() 
     {
-        if (isPerformingAction) return; // Skip Update logic during special actions
-
         distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
         canSeePlayer = eyeCollider.canSeePlayer;
 
-        print(canSeePlayer);
+        stateTimer += Time.deltaTime;
 
-        if (canSeePlayer && distanceToPlayer > 6) {
-            StartCoroutine(PrepareAndDash());
-        }
-        
-        if (canSeePlayer && distanceToPlayer <= 6 && !isPerformingAction && !colidedWithPlayer) {
-            print("Running");
-            animator.SetBool("isDashing", true);
-            FollowPlayer();
-        }
-        else{
-            animator.SetBool("isDashing", false);
-        }
-
-        if(!canSeePlayer && !colidedWithPlayer) {
-            print("Patrolling");
-            animator.SetBool("isWalking", true);
-            Patrol();
-        }
-        else {
-            animator.SetBool("isWalking", false);
-        }
-
-        if(colidedWithPlayer)
+        switch (state)
         {
-            StartCoroutine(StopAndWait());
+            case "patrol":
+                Patrol();
+                if (canSeePlayer && distanceToPlayer > 6) {
+                    state = "dash";
+                    stateTimer = 0f;
+                }
+                break;
+
+            case "dash":
+                Dash();
+                if (stateTimer >= dashDuration) {
+                    state = "grab";
+                    stateTimer = 0f;
+                }
+                break;
+
+            case "grab":
+                GrabAttack();
+                if (stateTimer >= 1f) {
+                    state = "patrol";
+                    stateTimer = 0f;
+                }
+                break;
+
+            case "stun":
+                Stun();
+                if (stateTimer >= 2f) {
+                    state = "patrol";
+                    stateTimer = 0f;
+                }
+                break;
+
+            default:
+                break;
         }
 
         if (gameObject.GetComponent<Rigidbody2D>().velocity.x != 0 && timer >= interval)
         {
-            dustParticles.Play(); // Reproduz as part�culas
+            dustParticles.Play(); // Reproduz as partículas
             timer = 0f; // Reseta o temporizador   
-        }
-
-    }
-
-    public void AttackPlayer() 
-    {
-        if (distanceToPlayer <= 5 && !hasDashed) {
-            StartCoroutine(PrepareAndDash());
-        }
-        else if (distanceToPlayer > 5 || hasDashed) {
-            FollowPlayer();
-        }
-    }
-
-    void FollowPlayer() 
-    {
-        Vector2 playerDirection = (player.position - transform.position).normalized;
-        float newDirection = playerDirection.x > 0 ? 1 : -1; // Determine the facing direction
-        rb.velocity = new Vector2(runningSpeed * playerDirection.x, rb.velocity.y);
-
-
-        // Flip the sprite if necessary
-        if (newDirection != direction)
-        {
-            direction = (int)newDirection;
-            Flip(direction);
         }
     }
 
     private void Patrol()
     {
+        animator.SetBool("isWalking", true);
         int layer = LayerMask.GetMask("Ground", "Destructable");
         
         // Check for obstacles in the current direction
@@ -134,93 +117,33 @@ public class Manti : Enemy
 
         // Apply movement based on the current direction
         rb.velocity = new Vector2(patrolSpeed * direction, rb.velocity.y);
-
     }
 
-    private IEnumerator PrepareAndDash() 
+    private void Dash()
     {
-        isPerformingAction = true;
-    
-        // Record the player's position before charging
-        playerLastKnownPosition = player.position;
-    
-        // Calculate dash direction (X-axis only)
-        Vector2 dashDirection = new Vector2(playerLastKnownPosition.x - transform.position.x, 0).normalized;
-    
-        float direction = dashDirection.x > 0 ? 1 : -1; // Determine facing direction
-    
-        // Stop and wait before charging
-        rb.velocity = Vector2.zero;
-        yield return new WaitForSeconds(1); // Wait before charging
         animator.SetBool("isDashing", true);
-    
-        // Gradual acceleration variables
-        float elapsedTime = 0f;
-        float chargeAccelerationTime = 0.1f; // Time it takes to reach max speed
-        float currentSpeed = 0f;
-    
-        // Gradually increase speed
-        while (elapsedTime < chargeAccelerationTime) 
-        {
-            elapsedTime += Time.deltaTime;
-            currentSpeed = Mathf.Lerp(0, dashSpeed, elapsedTime / chargeAccelerationTime); // Gradually interpolate speed
-            rb.velocity = dashDirection * currentSpeed;
-            yield return null;
-        }
-    
-        // Maintain max speed for the remaining dash duration
+        Vector2 dashDirection = new Vector2(player.position.x - transform.position.x, 0).normalized;
         rb.velocity = dashDirection * dashSpeed;
-    
-        yield return new WaitForSeconds(dashDuration - chargeAccelerationTime);
-    
-        animator.SetBool("isDashing", false);
-    
-        // Stop and wait after charging
-        rb.velocity = Vector2.zero;
-        yield return new WaitForSeconds(1); // Wait before flipping
-        Flip(direction);
-    
-        // Reset state
-        isPerformingAction = false;
-        hasDashed = true;
-    
-        // Check if the player is close for grabbing attack
-        if (distanceToPlayer <= 2) 
-        {
-            StartCoroutine(GrabAttack());
-        } 
-        else 
-        {
-            StartCoroutine(PrepareAndDash());
-        }
-    }
-    
-    public IEnumerator GrabAttack() 
-    {
-        isPerformingAction = true;
-        animator.SetBool("isGrabbing", true);
-        yield return new WaitForSeconds(0.5f); // Time for grabbing animation
-    
-        animator.SetBool("isGrabbing", false);
-        animator.SetBool("isGrabAttacking", true);
-        yield return new WaitForSeconds(0.5f); // Time for grab attack animation
-    
-        animator.SetBool("isGrabAttacking", false);
-        isPerformingAction = false;
     }
 
-    public IEnumerator StopAndWait() {
+    private void GrabAttack()
+    {
+        animator.SetBool("isGrabbing", true);
         rb.velocity = Vector2.zero;
-        yield return new WaitForSeconds(2);
-        colidedWithPlayer = false;
+    }
+
+    private void Stun()
+    {
+        rb.velocity = Vector2.zero;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
             colidedWithPlayer = true;
+            state = "stun";
+            stateTimer = 0f;
         }
     }
-
 
     private void Flip(float movementDirection)
     {
@@ -228,5 +151,4 @@ public class Manti : Enemy
         localScale.x *= -1;
         transform.localScale = localScale;
     }
-
 }
